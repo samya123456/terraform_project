0e557dba213a086fd1c7b648c7eb27641f997500
samya123456 <samya.nandy@gmail.com>
2023-05-24 23:44:13 +0530

    final peremeterized changes

--- test-samya/script/create_application.sh
+++ test-samya/script/create_application.sh
@@ -7,36 +7,44 @@
 #   esac
 # done
 
-# appName=App
-# routesdesc
-# contents="$(jq '.description = "App1"' json/createopenvpn_service_req.json)"
-# echo -E "${contents}" > json/createopenvpn_service_req.json
-
-# contents="$(jq '.routes[].description = "samya.com"' json/createopenvpn_service_req.json)"
-# echo -E "${contents}" > json/createopenvpn_service_req.json
-
-# contents="$(jq '.routes[].value = "samya.com"' json/createopenvpn_service_req.json)"
-# echo -E "${contents}" > json/createopenvpn_service_req.json
-
-
-# echo "branch_name = $branch_name"
+appName=App
+routesdesc=nandy.com
+routeval=nandy.com
+description=App
+name=App
 client_id="punGyMf71UnfT74vwVLYkIPzncNry1ZC.samyanandy"
 client_secret="uUgvlvsydP15hiDnCnutK5UQNrVRZmCfjy5qrlIIA7onmEffTqEfIMaVwWUzppfN"
 grant_type="client_credentials"
 networkItemId="dd5eccd2-0a35-4c3f-8b68-594ce2020c8f"
 networkItemType="NETWORK"
-
 get_token_url="https://samyanandy.api.openvpn.com/api/beta/oauth/token?client_id=$client_id&client_secret=$client_secret&grant_type=client_credentials"
-echo "get_token_url = $get_token_url"
+create_application_url="https://samyanandy.api.openvpn.com/api/beta/services?networkItemId=$networkItemId&networkItemType=$networkItemType"
 
+
+##Edit the request json##
+contents="$(jq --arg description $description '.description = $description' json/createopenvpn_service_req.json)"
+echo -E "${contents}" > json/createopenvpn_service_req.json
+
+contents="$(jq --arg name $name '.name = $name' json/createopenvpn_service_req.json)"
+echo -E "${contents}" > json/createopenvpn_service_req.json
+
+contents="$(jq --arg routesdesc $routesdesc '.routes[].description = $routesdesc' json/createopenvpn_service_req.json)"
+echo -E "${contents}" > json/createopenvpn_service_req.json
+
+contents="$(jq --arg routeval $routeval '.routes[].value =$routeval' json/createopenvpn_service_req.json)"
+echo -E "${contents}" > json/createopenvpn_service_req.json
+
+
+
+
+
+
+##Get the Access Token##
 curl -X POST $get_token_url -H "accept: application/json"  > json/out.json
 access_token=($(jq -r '.access_token'  ./json/out.json))
 echo "access_token = $access_token"
 
-create_application_url="https://samyanandy.api.openvpn.com/api/beta/services?networkItemId=$networkItemId&networkItemType=$networkItemType"
-
-echo "create_application_url = $create_application_url"
-
+##Create The application##
 res=$(curl -X POST $create_application_url \
   --write-out "%{http_code}" \
   -H "accept: application/json" \
